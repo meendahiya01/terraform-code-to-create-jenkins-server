02968ff6a5e1c9132721a1c89d3ba455bf6c8f66
Meenakshi Dahiya <meen.dahiya01@gmail.com>
2025-04-15 16:18:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app"
+    bucket = "mydev-project-terraform-sample-meenakshi-dahiya"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
